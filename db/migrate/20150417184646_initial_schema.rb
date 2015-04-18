2c8b3150aa8be1ac948f50b8fa9b810e851dd2e2
Kazuki Hasegawa <hasegawa@khasegawa.net>
2015-04-19 02:35:38 +0900

    add category_id index to postoss_posts

--- db/migrate/20150417184646_initial_schema.rb
+++ db/migrate/20150417184646_initial_schema.rb
@@ -19,6 +19,7 @@ class InitialSchema < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    add_index :postoss_posts, :category_id
     add_index :postoss_posts, :unique_id, unique: true
     add_foreign_key :postoss_posts, :categories
 
